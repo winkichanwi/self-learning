2e52848cdea3c81628d630f191240c86382a5e0d
Wing In CHAN <wingin.chan@bizreach.co.jp>
2018-10-01 11:30:48 +0900

    Future sequence

--- self-learning/src/main/scala/selflearning/FutureTraining.scala
+++ self-learning/src/main/scala/selflearning/FutureTraining.scala
@@ -1,5 +1,42 @@
 package selflearning
 
-class FutureTraining {
+import scala.concurrent.Future
+import scala.util.Success
+import scala.util.Failure
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.ExecutionContext.Implicits.global
 
+object FutureTraining extends App {
+    println("test - start")
+    val x1 = Future {
+        Thread.sleep(2000)
+        println("x1 is completed")
+        "Hello x1"
+    }
+
+    val x2 = Future {
+        Thread.sleep(4000)
+        println("x2 is completed")
+        throw new Exception("Exception occured with x2")
+    }
+
+    val x3 = Future {
+        Thread.sleep(10000)
+        println("x3 is completed")
+        "Hello x3"
+    }
+
+    val x = Future.sequence(List(x1, x2, x3))
+
+    x.onComplete {
+        case Success(res) => println("Success: " + res)
+        case Failure(ex)  => println("Ohhh Exception: " + ex.getMessage)
+    }
+
+    Await.ready(x, 20.seconds)
+
+    println(s"Future val is: ${x.value}")
+    Thread.sleep(2000)
+    println("test - end")
 }
