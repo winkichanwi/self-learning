74beaa28d5e434a7bddbd7f2c95da3e73b431f12
Winki Chan <chanwingin02@gmail.com>
2018-12-05 13:09:03 +0900

    MyList reverse and :::

--- self-learning/src/main/scala/mylist/MyList.scala
+++ self-learning/src/main/scala/mylist/MyList.scala
@@ -2,12 +2,22 @@ package mylist
 
 trait MyList[+A] {
   def head: A
+
   def tail: MyList[A]
+
   def isEmpty: Boolean
-//  def ::():  MyList[A]
-//  def :::(): MyList[A]
+
+  def ::[C >: A](e: C):  MyList[C]
+
+  def :::[C >: A](list: MyList[C]): MyList[C]
+
   def map[B](f: A => B): MyList[B]
+
   def flatMap[B](f: A => MyList[B]): MyList[B]
+
+  def reverse: MyList[A]
+
+  def foldLeft[B](zero: B)(f: (B, A) => B): MyList[B]
 }
 
 case class MyCons[A](head: A, tail: MyList[A]) extends MyList[A] {
@@ -15,7 +25,35 @@ case class MyCons[A](head: A, tail: MyList[A]) extends MyList[A] {
 
   override def map[B](f: A => B): MyList[B] = MyCons(f(head), tail.map(f))
 
-  override def flatMap[B](f: A => MyList[B]): MyList[B] = MyCons(f(head).head: B, tail.flatMap(f))
+  override def flatMap[B](f: A => MyList[B]): MyList[B] = {
+    f(head) match {
+      case MyCons(h, _) => MyCons(h, tail.flatMap(f))
+      case MyNil => MyNil
+    }
+  }
+
+  override def ::[C >: A](e: C): MyList[C] = MyCons(e, this)
+
+  override def :::[C >: A](list: MyList[C]): MyList[C] = {
+    if(!list.isEmpty) {
+      list.head :: list.tail ::: this
+    } else {
+      this
+    }
+  }
+
+  override def reverse: MyList[A] = {
+    def _append(result: MyList[A], addList: MyList[A]): MyList[A] = {
+      if (!addList.isEmpty) {
+        _append(addList.head :: result, addList.tail)
+      } else {
+        result
+      }
+    }
+    _append(MyNil, this)
+  }
+
+  override def foldLeft[B](zero: B)(f: (B, A) => B): MyList[B] = ???
 }
 
 case object MyNil extends MyList[Nothing] {
@@ -26,4 +64,12 @@ case object MyNil extends MyList[Nothing] {
   override def map[B](f: Nothing => B): MyList[B] = MyNil
 
   override def flatMap[B](f: Nothing => MyList[B]): MyList[B] = MyNil
+
+  override def ::[C >: Nothing](e: C): MyList[C] = MyCons(e, this)
+
+  override def :::[C >: Nothing](list: MyList[C]): MyList[C] = list
+
+  override def reverse: MyList[Nothing] = this
+
+  override def foldLeft[B](zero: B)(f: (B, Nothing) => B): MyList[B] = ???
 }
--- self-learning/src/test/scala/mylist/MyListSpec.scala
+++ self-learning/src/test/scala/mylist/MyListSpec.scala
@@ -42,5 +42,40 @@ class MyListSpec extends WordSpec {
       assert(result == MyCons(3, MyCons(4, MyCons(5, MyNil))))
     }
 
+    ":::" in {
+      // given
+      val list1 = MyCons(1, MyCons(2, MyCons(3, MyNil)))
+      val list2 = MyCons(4, MyCons(5, MyCons(6, MyNil)))
+
+      // when
+      val result = list1 ::: list2
+
+      // then
+      assert(result == MyCons(1, MyCons(2, MyCons(3, MyCons(4, MyCons(5, MyCons(6, MyNil)))))))
+    }
+    "Nil :::" in {
+      // given
+      val list1 = MyNil
+      val list2 = MyCons(4, MyCons(5, MyCons(6, MyNil)))
+
+      // when
+      val result = list1 ::: list2
+
+      // then
+      assert(result == MyCons(4, MyCons(5, MyCons(6, MyNil))))
+
+    }
+
+    "reverse" in {
+      // given
+      val list1 = MyCons(1, MyCons(2, MyCons(3, MyNil)))
+
+      // when
+      val result = list1.reverse
+
+      // then
+      assert(result == MyCons(3, MyCons(2, MyCons(1, MyNil))))
+    }
+
   }
 }
